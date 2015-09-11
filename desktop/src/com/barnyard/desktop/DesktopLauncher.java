51f47c92195bdbf2fbcfca1c2c6d877ceac94001
Trevor Hamilton <tlhamilt@mtu.edu>
2015-09-11 18:28:03 -0400

    Added gradle and other source files

---
+++ desktop/src/com/barnyard/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.barnyard.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.barnyard.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
