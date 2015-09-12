61fbb0c4e9651f28e1cf965f2b3713222b1e6bad
Eric Celerin <Ecceleri@mtu.edu>
2015-09-11 22:47:02 -0400

    Eric Test

--- core/src/com/barnyard/MyGame.java
+++ core/src/com/barnyard/MyGame.java
@@ -18,10 +18,10 @@ public class MyGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, .5f, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 50, 50);
+		batch.draw(img, 150, 150);
 		batch.end();
 	}
 }
