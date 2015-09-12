920269ed667f30c07f8756036c221a7f81dc444f
dnwing <dnwing@mtu.edu>
2015-09-12 17:30:36 -0400

    Changed color

--- core/src/com/barnyard/MyGame.java
+++ core/src/com/barnyard/MyGame.java
@@ -18,10 +18,10 @@ public class MyGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 150, 150);
+		batch.draw(img, 175, 125);
 		batch.end();
 	}
 }
