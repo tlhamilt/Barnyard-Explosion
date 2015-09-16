545b79cbff0057030d95375ec7cff60677679248
magryzwa <magryzwa@mtu.edu>
2015-09-16 16:19:42 -0400

    I added a comment into the main

--- core/src/com/barnyard/MyGame.java
+++ core/src/com/barnyard/MyGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+//This is a comment
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
